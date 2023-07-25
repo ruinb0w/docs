abe2dde27c3a88f8d4e511ef54d74ac47008b3e2
ruinb0w <ruinb0w@qq.com>
2023-07-25 15:56:56 +0800

    build

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Ruinb0w's Doc",
   description: "Some Documents",
+  ignoreDeadLinks: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [

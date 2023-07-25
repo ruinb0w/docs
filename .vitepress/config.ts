ee2a9f2d6e98c43266c568fe7344a9f989782951
ruinb0w <ruinb0w@qq.com>
2023-07-25 16:13:43 +0800

    update config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   description: "Some Documents",
   ignoreDeadLinks: true,
   outDir: "./docs",
+  base: "/docs/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
---
+++ docs/assets/README.md.a5baa5b7.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,k as o}from"./chunks/framework.93795505.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),s={name:"README.md"},c=o("p",null,"docs",-1),n=[c];function r(_,d,i,p,l,m){return a(),t("div",null,n)}const h=e(s,[["render",r]]);export{f as __pageData,h as default};
